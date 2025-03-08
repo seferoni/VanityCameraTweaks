26556a72590260230cb3888fd596a1f3725bd34a
Asim Seth <131764979+seferoni@users.noreply.github.com>
2025-03-08 23:37:46 +0630

    Initial commit.

---
+++ Framework/Harmony/Patches.cs
@@ -0,0 +1,61 @@
+﻿#region using directives
+
+using System.Reflection;
+using Kingmaker.UI.ServiceWindow;
+using UnityEngine.EventSystems;
+
+# endregion
+
+namespace VanityCameraTweaks.Harmony;
+
+[HarmonyPatch]
+internal class Patches
+{
+	[HarmonyPatch(typeof(DollRoomCharacterController), "OnScroll")]
+	[HarmonyPrefix]
+	internal static void PrefixOnScroll(PointerEventData eventData)
+	{
+		ModEntry.Log("OnScroll called!");
+		ModEntry.Log(string.Format("Got a delta-y of {0}.", eventData.scrollDelta.y));
+		ModEntry.Log(string.Format("Got a DollCamera state of {0}.", (bool)DollCamera.Current));
+	}
+
+	[HarmonyPatch(typeof(DollRoom), "Show")]
+	[HarmonyPostfix]
+	internal static void PostfixOnShow(bool visible, DollRoom __instance)
+	{
+		if (!visible)
+		{
+			ModEntry.Log("Hiding DollRoom.");
+			return;
+		}
+
+		FieldInfo cameraField = typeof(DollRoom).GetField("m_Camera", BindingFlags.NonPublic | BindingFlags.Instance);
+
+		if (cameraField is null)
+		{
+			ModEntry.Log("Could not find m_Camera.");
+			return;
+		}
+
+		DollCamera cameraInstance = (DollCamera)cameraField.GetValue(__instance);
+
+		if (cameraInstance is null)
+		{
+			ModEntry.Log("m_Camera is null.");
+			return;
+		}
+
+		ModEntry.Log("Camera instance found!");
+		ModEntry.Log(string.Format("Got Camera coordinates as [{0}, {1}, {2}]", cameraInstance.transform.position.x, cameraInstance.transform.position.y, cameraInstance.transform.position.z));
+		Vector3 newCoords = new Vector3
+		(
+			cameraInstance.transform.position.x, // 999.85f
+			cameraInstance.transform.position.y, // 586.1129f
+			cameraInstance.transform.position.z  // 0.987f
+		);
+		// TODO: consider excluding these translations during character creation
+		// TODO: consider adapting these translations for each character race
+		cameraInstance.transform.position = newCoords;
+	}
+};
---
+++ ModEntry.cs
@@ -0,0 +1,53 @@
+﻿# region global using directives
+
+global using System;
+global using System.Collections.Generic;
+global using UnityEngine;
+global using UnityModManagerNet;
+global using UnityEngine.UI;
+global using HarmonyLib;
+
+#endregion
+
+namespace VanityCameraTweaks;
+
+internal static class ModEntry
+{
+	internal static bool ModEnabledState { get; set; } = false;
+	internal static UnityModManager.ModEntry.ModLogger Logger { get; set; } = null!;
+	internal static HarmonyLib.Harmony HarmonyInstance { get; set; } = null!;
+
+	internal static void Log(string message)
+	{
+		Logger?.Log(message);
+	}
+
+	internal static void LogError(Exception exception)
+	{
+		Logger?.Log(string.Format("{0} \n {1}", exception.ToString(), exception.StackTrace));
+	}
+
+	static bool Load(UnityModManager.ModEntry modEntry)
+	{
+		try
+		{
+			modEntry.OnToggle = OnToggle;
+			Logger = modEntry.Logger;
+			HarmonyInstance = new HarmonyLib.Harmony(modEntry.Info.Id);
+			HarmonyInstance.PatchAll();
+		}
+		catch(Exception exception)
+		{
+			LogError(exception);
+			throw (exception);
+		}
+		
+		return true;
+	}
+
+	internal static bool OnToggle(UnityModManager.ModEntry modEntry, bool toggleState)
+	{
+		ModEnabledState = toggleState;
+		return true;
+	}
+};
